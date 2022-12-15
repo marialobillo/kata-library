4f044f4f88dcaec03e4ad5aa345a8e043c1a1157
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2022-12-15 13:05:16 +0100

    test: average method

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,10 +1,19 @@
-import { sum } from './stats'
+import { sum, average } from './stats'
 
-const result = sum([1, 2, 3])
-const expected = 6
+let result = sum([1, 2, 3])
+let expected = 6
 
 if(result === expected) {
-  console.log("Ok")
+  console.log("Ok - The sum() test is passing")
 } else {
-  throw new Error(`X ${result} is not equal to ${expected}`)
+  throw new Error(`X ${result} for sum() is not equal to ${expected}`)
+}
+
+result = average([1, 2, 3])
+expected = 2
+
+if (result === expected) {
+  console.log("Ok - The average() test is passing")
+} else {
+  throw new Error(`X ${result} for average() is not equal to ${expected}`)
 }
