61f0a1ff78e9da55c8a52d929f6e33f1b67f9ca9
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2022-12-15 13:43:20 +0100

    refactor: implementing test for testing

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,14 +1,20 @@
 import { sum, average } from './stats'
-import { expect } from './testLib'
+import { expect, test } from './testLib'
 
-let result = sum([1, 2, 3])
-let expected = 6
-expect(expected).toBe(result)
+let result, expected
+test("Calculate the sum of elements in an array", () => {
+  result = sum([1, 2, 3])
+  let expected = 6
+  expect(expected).toBe(result)
+})
 
 
-result = average([1, 2, 3])
-expected = 2
-expect(expected).toBe(result)
+test("Calculate the average of elments in an array", () => {
+  result = average([1, 2, 3])
+  let expected = 2
+  expect(expected).toBe(result)
+})
+
 
 
 
--- src/testLib.ts
+++ src/testLib.ts
@@ -1,10 +1,19 @@
 export function expect<T>(expected: T) {
   return {
     toBe<U extends T>(result: U) {
-      if (result === expected)
-        console.log("✅ ");
-      else
-        throw new Error(`❌ ${result} is not equal to ${expected}`);
+      if (result !== expected) {
+        throw new Error(`❌ ${result} is not equal to ${expected}`)
+      }
     }
+  }
+}
+
+export function test(description: string, callback: () => void) {
+  try {
+    callback()
+    console.log(`✅ ${description}`)
+  } catch (error) {
+    console.log(`❌ ${description}`)
+    console.log(error)
   }
 }
